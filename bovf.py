1ca3608a30e4f8e8f284bf10d7c1c4960b677996
Victor Carrico <victorcarrico@gmail.com>
2015-11-28 19:37:36 -0300

    BoVF class completed

--- bovf.py
+++ bovf.py
@@ -8,10 +8,13 @@ from sklearn.cluster import KMeans
 
 
 class BoVF(object):
-    def __init__(self, images, vocabulary_size=60):
-        self.images = images
+    def __init__(self, labeled_images, vocabulary_size=60):
+        self.images = labeled_images
         self.vocabulary_size = vocabulary_size
         self.descriptors = None
+        # self.classified_descriptors = {}
+        self.image_descriptors = []
+        self.histogram_list = []
 
     def _get_descriptor(self, image):
         img = cv2.imread(image)
@@ -21,17 +24,24 @@ class BoVF(object):
         return des
 
     def _get_descriptors(self):
-        for image in self.images:
-            des = self._get_descriptor(image)
-            if self.descriptors:
-                np.concatenate((self.descriptors, des))
+        for image, label in self.images.iteritems():
+            des = self._get_descriptor('dbNudeDetection/{}'.format(image))
+            self.image_descriptors.append(({image: label}, des, {}))
+            if self.descriptors is not None:
+                self.descriptors = np.concatenate((self.descriptors, des))
             else:
                 self.descriptors = des
 
     def clusterize(self):
-        kmeans = KMeans(n_clusters=self.vocabulary_size)
-        kmeans.fit(self.descriptors)
+        self.kmeans = KMeans(n_clusters=self.vocabulary_size)
+        self.kmeans.fit(self.descriptors)
+        # for descriptor in self.descriptors:
+        #     self.classified_descriptors.update(
+        #         {descriptor: self.kmeans.predict(descriptor)})
 
     def histogram(self):
-        # TODO
-        pass
+        for im in self.image_descriptors:
+            for descriptor in im[1]:
+                cluster = self.kmeans.predict(descriptor)
+                occur = im[2].get(cluster[0])
+                im[2].update({cluster[0]: occur + 1})
